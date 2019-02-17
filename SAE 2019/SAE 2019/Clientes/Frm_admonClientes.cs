54f9f759aa4ec4ab3ecab048f05d9a768c4a7f17
Allan Letona <aletonam@miumg.edu.gt>
2019-02-17 13:15:45 -0600

    DLL,Navegador y Formularios Maestros

--- SAE 2019/SAE 2019/Clientes/Frm_admonClientes.cs
+++ SAE 2019/SAE 2019/Clientes/Frm_admonClientes.cs
@@ -7,14 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaDiseno;
 
 namespace SAE_2019.Clientes
 {
     public partial class Frm_admonClientes : Form
     {
-        public Frm_admonClientes()
+        Navegador nv2 = new Navegador();
+        public Frm_admonClientes(DataGridView dgr)
         {
             InitializeComponent();
+            nv2.nombreForm(this);
+            nv2.dgv_datos(dgr);
         }
 
         private void Btn_cerrar_Click(object sender, EventArgs e)
@@ -25,6 +29,16 @@ namespace SAE_2019.Clientes
         private void Btn_minimizar_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Gpb_clientes_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Frm_admonClientes_Load(object sender, EventArgs e)
+        {
+            nv2.ingresarTabla("tbl_clientes");
         }
     }
 }
---
+++ SAE 2019/SAE 2019/Proveedores/Frm_pagoProveedores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SAE_2019.Proveedores
+{
+    public partial class Frm_pagoProveedores : Form
+    {
+        public Frm_pagoProveedores()
+        {
+            InitializeComponent();
+        }
+    }
+}
